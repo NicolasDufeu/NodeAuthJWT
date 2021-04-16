24cf7287e8883ac6a0396f5ad29f513c2aca9cc8
NicolasDufeu <nicoco-78@hotmail.fr>
2021-04-16 12:25:26 +0200

    sauvegarde

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    jwtSecret: 'ma-super-phrase-secrete-pour-gerer-le-json-web-token',
+    db: 'mongodb+srv://Nicolas:salut@cluster0.zlkra.mongodb.net/AUTH_DB?retryWrites=true&w=majority'
+}
